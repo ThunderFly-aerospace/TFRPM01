abc9e068cb935e209601117db8f923e45d005108
Roman Dvorak <romandvorak@mlab.cz>
2024-02-08 02:29:17 +0100

    new interactive QC tool

--- sw/README.md
+++ sw/README.md
@@ -10,3 +10,33 @@ Expected Linux kernel module: i2c_dev
 Set Bus and Address in source code.
 
     gcc rtc_1.c i2c_functions.c
+
+
+
+# TFRPM01 interactive testing utility
+
+```
+cd pymlab_interactive
+python3 quality_check.py --port 7 --address 0x50 --req_freq 1000 --max_deviation 500
+```
+
+## Prerequirements 
+* python3
+* pymlab
+* curses
+* argparse 
+
+
+## Usage: 
+
+```
+python3 quality_check.py --port 7 --address 0x50 --req_freq 1000 --max_deviation 500
+```
+
+`--port` smbus port 
+`--address` I2C address of TFRPM counter
+`--req_freq` 
+`--max_deviation`
+
+## Screenshots
+
---
+++ sw/pymlab_interactive/quality_check.py
@@ -0,0 +1,132 @@
+import curses
+import time, datetime
+from pymlab import config
+import argparse
+
+
+# Zpracování vstupních argumentů
+parser = argparse.ArgumentParser(description='Callibration utility for TFRPM01 sensor')
+parser.add_argument('--port', type=str, required=True, help='I2C port, e.g. /dev/i2c-1')
+parser.add_argument('--address', type=lambda x: int(x, 0), required=True, help='I2C sensor address. e.g. 0x50')
+parser.add_argument('--req_freq', type=int, required=True, help='Required frequency in Hz')
+parser.add_argument('--max_deviation', type=int, required=True, help='Maximum deviation in Hz')
+
+args = parser.parse_args()
+
+req_freq = args.req_freq
+max_deviation = args.max_deviation
+
+address = 0x50
+
+error = True
+connected = 0
+
+
+def main(screen):
+    global error, req_freq, max_deviation
+    
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_RED)
+
+    curses.cbreak()
+    curses.noecho()
+    screen.keypad(True)
+    screen.clear()
+
+    while True:
+        try:
+            print("Connecting...")
+            if error:
+                cfg = config.Config(
+                    i2c = {
+                        "port": args.port,
+                        #"driver": "smbus",
+                    },
+                    bus = [
+                        {
+                            "name": "TFRPM01",
+                            "type": "rtc01",
+                            "address": address
+                        }
+                    ]
+                )
+
+                cfg.initialize()
+                TFRPM01 = cfg.get_device("TFRPM01")
+                TFRPM01.initialize()
+                TFRPM01.set_config(TFRPM01.FUNCT_MODE_count)
+                TFRPM01.reset_counter()
+                count = TFRPM01.get_count()
+                old_range = None
+
+                connected = datetime.datetime.now()
+                error = False
+
+            while True:
+                count = TFRPM01.get_count()
+                freq = TFRPM01.get_frequency()
+                integration_time = TFRPM01.get_integration_time()
+                    
+                screen.clear()
+
+                screen.addstr(0, 0, "TFRPM01 validator")
+                screen.addstr(2, 0, f"Target frequency: {req_freq} HZ")
+                screen.addstr(3, 0, f"Accepted range {req_freq - max_deviation} - {req_freq + max_deviation} Hz")
+
+                screen.addstr(6, 0, f"Connected: {(datetime.datetime.now() - connected).seconds} s")
+                screen.addstr(7, 0, f"Count: {count}")
+                screen.addstr(8, 0, f"Frequency: {freq} Hz")
+                screen.addstr(9, 0, f"Integration Time: {integration_time:.2f} s")
+
+                in_range = bool(req_freq - max_deviation <= freq <= req_freq + max_deviation)
+                if in_range:
+                    screen.addstr(11, 0, "In range: OK", curses.color_pair(2))
+                else:
+                    screen.addstr(11, 0, "In range: ERROR", curses.color_pair(3))
+
+                screen.addstr(12, 0, "")
+                if old_range != in_range:
+                    old_range = in_range
+                    curses.beep()
+
+                screen.refresh()
+                time.sleep(0.2)
+
+                if count >= TFRPM01.MAX_COUNT / 2:
+                    TFRPM01.reset_counter()
+                    time.sleep(0.5)
+                    #screen.addstr(10, 0, "Counter reset due to half range exceeded")
+                if integration_time > 5:
+                    TFRPM01.reset_counter()
+                    time.sleep(0.5)
+                    #screen.addstr(10, 0, "Counter reset due to integration time exceeded")
+
+        except IOError:
+            print("ERROR... ")
+            if not error:
+                screen.clear()
+                screen.addstr(0, 0, f"IO ERROR...")
+                time.sleep(0.5)
+                
+                error = True
+
+        except Exception as e:
+            print("ERROR... ", e)
+            if not error:
+                screen.clear()
+                screen.addstr(0, 0, f"IO ERROR...")
+                time.sleep(0.5)
+                
+                error = True
+
+
+    
+    curses.nocbreak()
+    screen.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+if __name__ == "__main__":
+    curses.wrapper(main)
